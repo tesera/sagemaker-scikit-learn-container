0232b7fbdc569d1d60ad0572a4944fea5cdc271e
Max Turgeon <max.turgeon@tesera.com>
2024-02-13 14:50:29 -0500

    update sklearn version

---
+++ config/config_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo export CONTAINER=tesera/sagemaker-scikit-learn
+echo export ACCOUNT=940145619248
+echo export BUILD_ACCOUNT=712635442529
+echo export REGION=us-west-2
+echo export PROFILE=hris-development
+echo export BUILD_PROFILE=hris-workflows
